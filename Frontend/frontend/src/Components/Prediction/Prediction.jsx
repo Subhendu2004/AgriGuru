329557e4c9023da10c4625346b1e55b71284e02d
Subhend Das <143273015+Subhendu2004@users.noreply.github.com>
2025-07-20 16:55:52 +0530

    Update Prediction.jsx

--- Frontend/frontend/src/Components/Prediction/Prediction.jsx
+++ Frontend/frontend/src/Components/Prediction/Prediction.jsx
@@ -27,7 +27,7 @@ const Prediction = () => {
     );
 
     try {
-      const response = await axios.post("http://127.0.0.1:5000/predict", numericFormData);
+      const response = await axios.post("https://agriguru-10mi.onrender.com/predict", numericFormData);
       setPrediction(response.data.prediction);
     } catch (error) {
       console.error("Prediction error:", error);
